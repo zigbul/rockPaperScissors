e5ec13827cb65393d100346e3f366e9db21359f0
zigbul <65498093+zigbul@users.noreply.github.com>
2024-08-21 21:39:00 +0300

    add results

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const possibleChoices = document.querySelectorAll('button');
 
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoices) =>
   possibleChoices.addEventListener('click', (e) => {
@@ -12,11 +13,28 @@ possibleChoices.forEach((possibleChoices) =>
     userChoiceDisplay.innerHTML = userChoice;
 
     generateComputerChoice();
+    getResults();
   }),
 );
 
 function generateComputerChoice() {
   const randomNumber = Math.floor(Math.random() * possibleChoices.length);
-  computerChoice = possibleChoices[randomNumber];
-  computerChoiceDisplay.innerHTML = computerChoice.id;
+  computerChoice = possibleChoices[randomNumber].id;
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResults() {
+  if (computerChoice === userChoice) {
+    result = "It's a draw!";
+  } else if (
+    (computerChoice === 'rock' && userChoice === 'scissors') ||
+    (computerChoice === 'paper' && userChoice === 'rock') ||
+    (computerChoice === 'scissors' && userChoice === 'paper')
+  ) {
+    result = 'Sorry, You lose!';
+  } else {
+    result = 'Hurray, You win!';
+  }
+
+  resultDisplay.innerHTML = result;
 }
