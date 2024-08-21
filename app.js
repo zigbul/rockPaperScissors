921ab67b55526bbbf88bf20468c534ce516419c1
zigbul <65498093+zigbul@users.noreply.github.com>
2024-08-21 21:17:16 +0300

    add computer choice

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 
 let userChoice;
+let computerChoice;
 
 possibleChoices.forEach((possibleChoices) =>
   possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+
+    generateComputerChoice();
   }),
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+  computerChoice = possibleChoices[randomNumber];
+  computerChoiceDisplay.innerHTML = computerChoice.id;
+}
